daddb48ec176fb584d464a19e60f1a8391d1bd97
Ander Duran Albiz <344059A@H015110.NET>
2026-02-06 17:12:25 +0100

    recorte en la zona de clases

--- 01-libreria_ejercicio/app/src/main/java/com/example/libreria_pmd/sharedModel/Libro.kt
+++ 01-libreria_ejercicio/app/src/main/java/com/example/libreria_pmd/sharedModel/Libro.kt
@@ -1,38 +1,5 @@
 package com.example.libreria
 
-interface Producto {
-    fun esCaro(): Boolean
-}
-
-open class Libro(
-    var titulo: String = "",
-    var publicacion: Int = 0,
-    var precio: Int = 0
-): Producto {
-
-    override fun esCaro():Boolean {
-        var resultado = false
-        if(this.precio>25) {
-            resultado = true
-        }
-        return resultado
-    }
-
-    override fun toString():String {
-        return "${this::class.simpleName} - titulo:${this.titulo},publicacion:${this.publicacion},precio:${this.precio}"
-    }
-}
-
-class LibroDigital(
-    titulo: String = "",
-    publicacion: Int = 0,
-    precio: Int = 0,
-    var sizeArchivoMB: Int = 0,
-    var formato: String = "PDF"
-) : Libro(titulo, publicacion, precio) {
-
-    override fun toString(): String {
-        return "${super.toString()}, tamañoArchivoMB:$sizeArchivoMB, formato:$formato"
-    }
-
-}
+//TODO crea aqui todas las clases
+// Dejo esta data class para que compile
+data class Libro(var nombre:String)
